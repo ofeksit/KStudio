21c7a6fbd9a01136c5271fee8d3b1fbb0ff918b2
ofeksit <ofek.sit@gmail.com>
2024-09-06 00:58:26 +0300

    Modify My trainings popup tab

--- KStudio/src/app/app.component.ts
+++ KStudio/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    // Force light theme on app startup
+    document.body.setAttribute('data-theme', 'light');
+  }
 }
