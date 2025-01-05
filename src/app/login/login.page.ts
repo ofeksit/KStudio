47ac4244eecfb5b88c2e508f3534585dbc116533
ofeksit <ofek.sit@gmail.com>
2025-01-05 21:06:29 +0200

    After android publish

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -66,99 +66,119 @@ export class LoginPage {
     this.isLoading = true;
 
     this.authService.login(this.username, this.password).pipe(
-        switchMap((response: any) => {
-
-            // Store token, ID, and email in local storage
-            this.authService.storeToken(response.data.token);
-            this.authService.storeUserID(response.data.id);
-            this.authService.storeUserEmail(response.data.email);
-
-            const headers = new HttpHeaders({
-                'Authorization': `Bearer ${response.data.token}`
-            });
-
-            // First request: Get user details
-            return this.http.get(`https://k-studio.co.il/wp-json/wp/v2/users/me`, { headers });
-        }),
-        switchMap((userDetails: any) => {
-
-            // Store user details in local storage
-            this.authService.storeUserFullName(userDetails.name);
-            this.authService.storeUserGamiPts(userDetails.meta._gamipress_pts_points);
-
-            const headers = new HttpHeaders({
-                'Authorization': `Bearer ${this.authService.getToken()}`
-            });
-
-            // Second request: Get user role
-            return this.http.get(`https://k-studio.co.il/wp-json/custom-api/v1/user-role/${userDetails.id}`, { headers });
-        }),
-        switchMap((userRoleResponse: any) => {
-
-            // Store user role in local storage
-            this.authService.storeUserRole(userRoleResponse.roles[0]);
-
-            // Now call the custom WordPress endpoint that interacts with the Amelia API
-            const userEmail = this.authService.getUserEmail();
-            const ameliaApiUrl = `https://k-studio.co.il/wp-json/custom-api/v1/amelia-customer-id?email=${userEmail}`;
-
-            return this.http.get(ameliaApiUrl); // Call the custom endpoint
-        }),
-        switchMap((ameliaResponse: any) => {
-
-            // Store the customer ID in local storage if the Amelia API call was successful
-            if (ameliaResponse && ameliaResponse.customerId) {
-                this.authService.storeCustomerID(ameliaResponse.customerId);
-            }
-
-            // Fetch packageCustomerId using the Amelia customerId
-            const packageApiUrl = `https://k-studio.co.il/wp-json/wn/v1/package-purchases/${ameliaResponse.customerId}`;
-            return this.http.get(packageApiUrl);
-        }),
-        switchMap((packageResponse: any) => {
-            console.log("package response:", packageResponse);
-            // Extract the packageCustomerId and store it in local storage
-            if (packageResponse && packageResponse.data && packageResponse.data[0] && 
-                packageResponse.data[0].packages[0] && 
-                packageResponse.data[0].packages[0].purchases[0] && 
-                packageResponse.data[0].packages[0].purchases[0].packageCustomerId) {
-                const packageCustomerId = packageResponse.data[0].packages[0].purchases[0].packageCustomerId;
-                console.log("packageID:", packageResponse.data[0].packages[0].purchases[0].packageCustomerId);
-                this.authService.storePackageCustomerID(packageCustomerId);
-            }
-
-            // Fetch favorite location
-            const favLocationApiUrl = `https://k-studio.co.il/wp-json/custom-api/v1/get-favorite-location?user_id=${this.authService.getUserID()}`;
-            return this.http.get(favLocationApiUrl);
-        }),
-        tap((favLocationResponse: any) => {
-          console.log("favlocation", favLocationResponse)
-          console.log("favlocation", favLocationResponse.favorite_location)
-            // Store favorite location in local storage
-            if (favLocationResponse && favLocationResponse.favorite_location) {
-                this.authService.storeFavLocation(favLocationResponse.favorite_location);
-            }
-        }),
-        catchError(error => {
-            console.error('Error occurred during login process:', error);
-            this.authService.logout(); // Ensure we clear the token
-
-            if (error.status === 401 || error.status === 403) {
-                // Incorrect username/password
-                this.errorMessage = 'שם משתמש או סיסמה שגויים.';
-                this.presentToast(this.errorMessage, 'danger');
-            } else {
-                // Handle all other errors (e.g., during data fetching)
-                this.errorMessage = 'אירעה שגיאה, אנא נסה שנית';
-                this.presentToast(this.errorMessage, 'medium'); // Grey alert
-            }
-
-            // Set loading state back to false on error
-            this.isLoading = false;
-
-            return throwError(error); // Propagate the error to stop the success flow
-        })
-    ).subscribe(
+      switchMap((response: any) => {
+          // Store token, ID, and email in local storage
+          this.authService.storeToken(response.data.token);
+          this.authService.storeUserID(response.data.id);
+          this.authService.storeUserEmail(response.data.email);
+  
+          const headers = new HttpHeaders({
+              'Authorization': `Bearer ${response.data.token}`
+          });
+  
+          // First request: Get user details
+          return this.http.get(`https://k-studio.co.il/wp-json/wp/v2/users/me`, { headers });
+      }),
+      switchMap((userDetails: any) => {
+          // Store user details in local storage
+          this.authService.storeUserFullName(userDetails.name);
+          this.authService.storeUserGamiPts(userDetails.meta._gamipress_pts_points);
+  
+          const headers = new HttpHeaders({
+              'Authorization': `Bearer ${this.authService.getToken()}`
+          });
+  
+          // Second request: Get user role
+          return this.http.get(`https://k-studio.co.il/wp-json/custom-api/v1/user-role/${userDetails.id}`, { headers });
+      }),
+      switchMap((userRoleResponse: any) => {
+          // Store user role in local storage
+          const userRole = userRoleResponse.roles[0];
+          this.authService.storeUserRole(userRole);
+  
+          // If user is administrator, skip Amelia API calls and return a dummy response
+          if (userRole === 'administrator') {
+              return of({ 
+                  skipAmelia: true,
+                  customerId: null,
+                  packages: []
+              });
+          }
+  
+          // For non-administrators, proceed with Amelia API calls
+          const userEmail = this.authService.getUserEmail();
+          const ameliaApiUrl = `https://k-studio.co.il/wp-json/custom-api/v1/amelia-customer-id?email=${userEmail}`;
+          return this.http.get(ameliaApiUrl);
+      }),
+      switchMap((ameliaResponse: any) => {
+          // Skip package fetching for administrators
+          if (ameliaResponse.skipAmelia) {
+              return of(ameliaResponse);
+          }
+  
+          // Store the customer ID in local storage if the Amelia API call was successful
+          if (ameliaResponse && ameliaResponse.customerId) {
+              this.authService.storeCustomerID(ameliaResponse.customerId);
+          }
+  
+          // Fetch packageCustomerId using the Amelia customerId
+          const packageApiUrl = `https://k-studio.co.il/wp-json/wn/v1/package-purchases/${ameliaResponse.customerId}`;
+          return this.http.get(packageApiUrl);
+      }),
+      switchMap((packageResponse: any) => {
+          // Skip for administrators
+          if (packageResponse.skipAmelia) {
+              return of(packageResponse);
+          }
+  
+          console.log("package response:", packageResponse);
+          // Extract the packageCustomerId and store it in local storage
+          if (packageResponse && packageResponse.data && packageResponse.data[0] && 
+              packageResponse.data[0].packages[0] && 
+              packageResponse.data[0].packages[0].purchases[0] && 
+              packageResponse.data[0].packages[0].purchases[0].packageCustomerId) {
+              const packageCustomerId = packageResponse.data[0].packages[0].purchases[0].packageCustomerId;
+              console.log("packageID:", packageCustomerId);
+              this.authService.storePackageCustomerID(packageCustomerId);
+          }
+  
+          // Fetch favorite location
+          const favLocationApiUrl = `https://k-studio.co.il/wp-json/custom-api/v1/get-favorite-location?user_id=${this.authService.getUserID()}`;
+          return this.http.get(favLocationApiUrl);
+      }),
+      tap((favLocationResponse: any) => {
+          // Skip for administrators
+          if (favLocationResponse.skipAmelia) {
+              return;
+          }
+  
+          console.log("favlocation", favLocationResponse);
+          console.log("favlocation", favLocationResponse.favorite_location);
+          // Store favorite location in local storage
+          if (favLocationResponse && favLocationResponse.favorite_location) {
+              this.authService.storeFavLocation(favLocationResponse.favorite_location);
+          }
+      }),
+      catchError(error => {
+          console.error('Error occurred during login process:', error);
+          this.authService.logout(); // Ensure we clear the token
+  
+          if (error.status === 401 || error.status === 403) {
+              // Incorrect username/password
+              this.errorMessage = 'שם משתמש או סיסמה שגויים.';
+              this.presentToast(this.errorMessage, 'danger');
+          } else {
+              // Handle all other errors (e.g., during data fetching)
+              this.errorMessage = 'אירעה שגיאה, אנא נסה שנית';
+              this.presentToast(this.errorMessage, 'medium'); // Grey alert
+          }
+  
+          // Set loading state back to false on error
+          this.isLoading = false;
+  
+          return throwError(error); // Propagate the error to stop the success flow
+      })
+  ).subscribe(
         async () => {
             // Show progress bar or navigate to home page after delay
             this.showProgressBar = true;
